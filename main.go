14f289a9ae488189081f1e046b9df997fba0ca8b
mark-080383 <080383@landbank.com.tw>
2026-04-08 17:44:59 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    _ "github.com/beego/beego/v2/server/web"
+    _ "github.com/fogleman/gg"
+    _ "github.com/google/uuid"
+    _ "github.com/jinzhu/now"
+    _ "github.com/satori/go.uuid"
+    _ "github.com/signintech/gopdf"
+    _ "github.com/xuri/excelize/v2"
+    _ "golang.org/x/text/encoding/traditionalchinese"
+    _ "gorm.io/driver/sqlserver"
+    _ "gorm.io/gorm"
+)
+
+func main() {}
